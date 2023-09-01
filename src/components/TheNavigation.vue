73e7ebd1dead123f8e1637b2711488b38c2ddbe1
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-09-01 17:17:22 +0800

    lesson 19 is done

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <router-link id="logo" to="/">Vue School Travel App</router-link>
+    <router-link v-for="destination in destinations" :key="destination.id" :to="{name: 'destination.show', params: {id: destination.id, slug: destination.slug}}">
+        <h2>{{ destination.name }}</h2>
+      </router-link>
+  </div>
+</template>
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  data() {
+    return {
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/destination/:id/:slug',
+    name: 'destination.show',
+    component: () => import(/* webpackChunkName: "destination" */"../views/DestinationShow.vue"),
+    props: route => ({...route.params, id: parseInt(route.params.id)}),
+    children: [
+      {
+        path: ':experienceSlug',
+        name: 'experience.show',
+        component: () => import('@/views/ExperienceShow.vue'),
+        props: route => ({...route.params, id: parseInt(route.params.id)})
+      }
+    ]
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
-
 export default router
